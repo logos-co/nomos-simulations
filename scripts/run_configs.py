02a82a421bd721a98d5da7203ab1990c571f2aad
gusto <bacv@users.noreply.github.com>
2023-08-16 11:11:16 +0300

    Run all configs without log parsing from simapp (#1)

--- scripts/run_configs.py
+++ scripts/run_configs.py
@@ -4,19 +4,9 @@ import os
 import time
 
 def run_simulation(command):
-    # TODO: Graceful shutdown in simulation doesn't work yet, remove the ouput filtering once implemented.
-    search_string = "ALL DONE"
-
-    with subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True, bufsize=1, universal_newlines=True) as process:
-        for line in iter(process.stdout.readline, ''):
-            if search_string in line:
-                # TODO: In simulation, subscriber will drop all remaining unpersisted records when the process
-                # is terminated, add some delay for filesystem to catch up with writing the output data.
-                time.sleep(5)
-                process.terminate()
-                return
-
-        process.communicate()  # wait for the process to finish if it hasn't yet
+    start_time = time.time()
+    process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True, bufsize=1, universal_newlines=True)
+    process.communicate()
 
 def run_simulations(configs_path):
     for filename in os.listdir(configs_path):
