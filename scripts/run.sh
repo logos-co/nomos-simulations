e6d29df9d43bc4cdc9e86c286bbfa9c167e8ade2
0xFugue <119708655+0xFugue@users.noreply.github.com>
2023-09-01 11:45:36 +0530

    run: rename json and clearout unncessary files

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,12 +1,12 @@
 
 #for i in "10 100 200 300 400 500 600 700 800 900 1000 2000 3000 4000 5000 6000 7000 8000 9000 10000"
 prefix="compare"
-for p in 0.8 0.5 0.1 0.01 0.001 0.0001
+for p in 0.8 0.5 #0.1 0.01 0.001 0.0001
 do 
   dir="compare_"$p"/"
   mkdir -p $dir 
   echo "overlay,nodes,committees_or_depth,description" >  $dir$prefix"_"$p".csv"
-  for i in 10 50 100 250 500 750 1000 2000 3000 4000 5000 6000 7000 8000 9000 10000 12000 1400
+  for i in 10 50 100 250 500 750 #1000 2000 3000 4000 5000 6000 7000 8000 9000 10000 12000 1400
   do
   python3 build_tests.py --num-nodes $i --failure-threshold $p >> $dir$prefix"_"$p".csv"
   echo "num-nodes = $i, failure-threshold = $p"
@@ -14,18 +14,34 @@ do
 done
 
 
-for p in 0.8 0.5 0.1 0.01 0.001 0.0001
+
+for p in 0.8 0.5 #0.1 0.01 0.001 0.0001
 do
   dir="compare_"$p"/"
   cd $dir
   mkdir configs output scripts
   cp  ../*.py  scripts
   cd scripts
-  ln -s ../../config_builder/   
+  ln -s ../../config_builder/
   python3 build_cases.py  ../$prefix"_"$p".csv"
-  python3 run_configs.py ../configs 
   cd ..
-  rm -rf scripts
+  python3 scripts/run_configs.py configs/
   cd ..
   echo "config gen ($p) done.."
+done
+
+
+
+for p in 0.8 0.5 #0.1 0.01 0.001 0.0001
+do
+   dir="compare_"$p"/"
+   cd $dir
+   pwd
+   ls -l output/*.json |  awk '{print $9}' > json_files
+   sed -e s/\.json//g -i json_files
+   for i in `cat json_files`
+   do
+      mv $i.json $i.csv
+   done
+   rm -rf  scripts
 done
