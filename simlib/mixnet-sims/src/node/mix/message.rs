7ff5b0300daf97f82949022e29d6085ffd6cc8d8
Youngjoon Lee <5462944+youngjoon-lee@users.noreply.github.com>
2024-11-08 11:44:22 +0700

    Use String for PayloadId

--- simlib/mixnet-sims/src/node/mix/message.rs
+++ simlib/mixnet-sims/src/node/mix/message.rs
@@ -1,24 +1,39 @@
 use uuid::Uuid;
 
-pub type PayloadId = [u8; 16];
+pub type PayloadId = String;
 
-pub struct Payload(PayloadId);
+pub struct Payload(Uuid);
 
 impl Payload {
     pub fn new() -> Self {
-        Self(Uuid::new_v4().into_bytes())
+        Self(Uuid::new_v4())
     }
 
     pub fn id(&self) -> PayloadId {
-        self.0
+        self.0.to_string()
     }
 
     pub fn as_bytes(&self) -> &[u8] {
-        &self.0
+        self.0.as_bytes()
     }
 
     pub fn load(data: Vec<u8>) -> Self {
         assert_eq!(data.len(), 16);
         Self(data.try_into().unwrap())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Payload;
+
+    #[test]
+    fn payload() {
+        let payload = Payload::new();
+        println!("{}", payload.id());
+        let bytes = payload.as_bytes();
+        assert_eq!(bytes.len(), 16);
+        let loaded_payload = Payload::load(bytes.to_vec());
+        assert_eq!(bytes, loaded_payload.as_bytes());
     }
 }
