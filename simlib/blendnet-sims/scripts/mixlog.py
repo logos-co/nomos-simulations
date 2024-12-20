1c0fd656c3f3c8944b6e1b72347a602728984832
Youngjoon Lee <5462944+youngjoon-lee@users.noreply.github.com>
2024-12-20 23:36:22 +0900

    embed topic in json

--- simlib/blendnet-sims/scripts/mixlog.py
+++ simlib/blendnet-sims/scripts/mixlog.py
@@ -3,20 +3,18 @@ import sys
 from collections.abc import Iterable
 from typing import Optional
 
-TOPIC_INDICATOR = "Topic:"
+TOPIC_LOG_INDICATOR = '{"topic":'
 
 
 def line_to_json_stream(record_stream: Iterable[str]) -> Iterable[tuple[str, dict]]:
     for record in record_stream:
-        topic_idx = record.find(TOPIC_INDICATOR)
+        topic_idx = record.find(TOPIC_LOG_INDICATOR)
         if topic_idx == -1:
             continue
 
         # Split the line into 2 parts: topic and JSON message
-        parts = record[topic_idx + len(TOPIC_INDICATOR) :].split(":", maxsplit=1)
-        topic = parts[0].strip()
-        json_record = json.loads(parts[1].strip())
-        yield (topic, json_record)
+        log = json.loads(record[topic_idx:].strip())
+        yield (log["topic"], log["message"])
 
 
 def get_pipe_stream() -> Iterable[str]:
--- simlib/blendnet-sims/src/node/blend/log.rs
+++ simlib/blendnet-sims/src/node/blend/log.rs
@@ -1,10 +1,21 @@
+use serde::Serialize;
+
 #[macro_export]
 macro_rules! log {
     ($topic:expr, $msg:expr) => {
         tracing::info!(
-            "Topic:{}: {}",
-            $topic,
-            serde_json::to_string(&$msg).unwrap()
+            "{}",
+            serde_json::to_string(&$crate::node::blend::log::TopicLog {
+                topic: $topic.to_string(),
+                message: $msg
+            })
+            .unwrap()
         );
     };
+}
+
+#[derive(Serialize)]
+pub struct TopicLog<M: Serialize> {
+    pub topic: String,
+    pub message: M,
 }
